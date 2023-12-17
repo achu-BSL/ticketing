cae37e8cbd9dcda6a2e8bf3f3b3a86e1daff2e05
achu-bsl <asharudeenalikt@gmail.com>
2023-12-17 15:44:26 +0530

    feat(signup): added user model and BadRequest Error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,17 +1,24 @@
-import express, { json } from "express";
-import "express-async-errors";
-import mongoose from "mongoose";
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
 
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all("*", async (req, res) => {
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
@@ -19,15 +26,16 @@ app.use(errorHandler);
 
 const start = async () => {
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
-    console.log('Mongodb connection established..');
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+
+    console.log('Connected to MongoDb');
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
+
   app.listen(3000, () => {
-    console.log("Listening on port 3000!!");
+    console.log('Listening on port 3000!!!!!!!!');
   });
 };
 
 start();
-
