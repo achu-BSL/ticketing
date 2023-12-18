1810b4933cfa7dc44c0d713cfd6f5ca89e535d27
achu-bsl <asharudeenalikt@gmail.com>
2023-12-18 12:49:54 +0530

    feat(signin, signout, current-user) Added signin, signout and current-user router

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,41 +2,46 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
-import { validateRequest } from '../middlewares/validate-request';
+import { Password } from '../services/password';
 import { User } from '../models/user';
+import { validateRequest } from '../middlewares/validate-request';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post(
-  '/api/users/signup',
+  '/api/users/signin',
   [
     body('email')
       .isEmail()
       .withMessage('Email must be valid'),
     body('password')
       .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4 and 20 characters')
+      .notEmpty()
+      .withMessage('You must supply a password')
   ],
   validateRequest,
   async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      throw new BadRequestError('Email in use');
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
     }
 
-    const user = User.build({ email, password });
-    await user.save();
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid Credentials');
+    }
 
     // Generate JWT
     const userJwt = jwt.sign(
       {
-        id: user.id,
-        email: user.email
+        id: existingUser.id,
+        email: existingUser.email
       },
       process.env.JWT_KEY!
     );
@@ -46,8 +51,8 @@ router.post(
       jwt: userJwt
     };
 
-    res.status(201).send(user);
+    res.status(200).send(existingUser);
   }
 );
 
-export { router as signupRouter };
+export { router as signinRouter };
